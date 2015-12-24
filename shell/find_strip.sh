832308491c9c608eaa9316f23271c8ffc5a01b47
choueric <zhssmail@gmail.com>
2015-12-24 10:29:49 +0800

    add another exlude method.

--- shell/find_strip.sh
+++ shell/find_strip.sh
@@ -7,5 +7,8 @@
 # - exlude name libpthread*.so* and *.ko
 
 FIND_PATH=/home/zhs/workspace/ti8107/svn/EmbPlatformDM8107/trunk/buildroot-2015.08.1/output/target
+EXLUDE_PATH=/home/zhs/workspace/ti8107/svn/EmbPlatformDM8107/trunk/buildroot-2015.08.1/output/target/opt/dvr_rdk*
 
 find $FIND_PATH -not \( -path '*dvr_rdk*' \)  -type f \( -perm /111 -o -name '*.so*' \) -not \( -name 'libpthread*.so*' -o -name '*.ko' \) 
+# or this one
+find $FIND_PATH  \( -path $EXLUDE_PATH \) -prune -o -type f \( -perm /111 -o -name '*.so*' \) -not \( -name 'libpthread*.so*' -o -name '*.ko' \) -print
