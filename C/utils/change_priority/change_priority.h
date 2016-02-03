cf7d1485be1e156dcd64b47f2bf49a5a4a5a6747
choueric <zhssmail@gmail.com>
2016-02-03 11:19:51 +0800

    update change_priority utility.

--- C/utils/change_priority/change_priority.h
+++ C/utils/change_priority/change_priority.h
@@ -1,6 +1,8 @@
 #ifndef _CHANGEPRIORITY_H_
 #define _CHANGEPRIORITY_H_
 
+int get_priority();
 
+int set_priority(int priority);
 #endif
 
--- C/utils/change_priority/test.c
+++ C/utils/change_priority/test.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
-#include "basename.h"
-
-#define INPUT_1 "/home/zhs/test"
-#define INPUT_2 "/home/zhs/test/main.c/"
-#define INPUT_3 "/home/zhs/test/main.c"
-
-void test(char *input, int size)
-{
-	char output[100] = {0};
-
-	basename_r(input, output, sizeof(output));
-
-	printf("-----------------------------------------\n");
-	printf("input is: [%s]\noutput is : [%s]\n", input, output);
-}
+#include <unistd.h>
+#include "change_priority.h"
 
 int main(int argc, char **argv)
 {
-	test(INPUT_1, sizeof(INPUT_1));
-	test(INPUT_2, sizeof(INPUT_2));
-	test(INPUT_3, sizeof(INPUT_3));
+	get_priority();
+
+	set_priority(66);
+
+	get_priority();
+
+	sleep(100);
 
 	return 0;
 }
