e694d2271074ad73f1e2dfbecdb35f4eb4971473
choueric <zhssmail@gmail.com>
2016-04-18 14:19:30 +0800

    update ape2mp3.sh

--- shell/ape2mp3.sh
+++ shell/ape2mp3.sh
@@ -10,7 +10,7 @@ if [[ $# < 1 ]];then
 	exit 1
 fi
 
-NAME=`basename $1 .ape`
+NAME=`basename "$1" .ape`
 CUENAME="${NAME}.cue"
 
 #ENCODE=`vim ${NAME}.cue  -c 'let $enc = &fileencoding | execute "silent !echo $enc" | q'`
@@ -18,10 +18,10 @@ ENCODE=gb18030
 
 echo "convert '$NAME'"
 
-avconv -i ${NAME}.ape -b 320k ${NAME}.mp3 
+avconv -i "${NAME}.ape" -b 320k "${NAME}.mp3"
 
 echo "cue file encoding: $ENCODE"
-iconv -f $ENCODE -t utf8 ${CUENAME} -o ${CUENAME}_utf8.cue
-mp3splt -a -c ${CUENAME}_utf8.cue ${NAME}.mp3 -o @n-@A-@t -d ${NAME}
+iconv -f "$ENCODE" -t utf8 "${CUENAME}" -o "${CUENAME}_utf8.cue"
+mp3splt -a -c "${CUENAME}_utf8.cue" "${NAME}.mp3" -o @n-@A-@t -d "${NAME}"
 
-rm ${NAME}.mp3
+rm "${NAME}.mp3"
