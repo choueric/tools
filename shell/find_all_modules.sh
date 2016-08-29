083fa328316a81a5f2bb284313ac24f21cf8f063
choueric <zhssmail@gmail.com>
2016-08-29 16:49:17 +0800

    add script finding all modules in current system.

---
+++ shell/find_all_modules.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# find_all_modules.sh
+#
+
+for i in `find /sys/ -name modalias -exec cat {} \;`; do
+	/sbin/modprobe --config /dev/null --show-depends $i ;
+done | rev | cut -f 1 -d '/' | rev | sort -u
