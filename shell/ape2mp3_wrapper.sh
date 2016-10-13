4988071958bd2a554df33196f3dd7090163e3f74
zhs <haishan.zhou@magewell.com>
2016-10-13 10:30:47 +0800

    add wrapper script for convert ape to mp3.

---
+++ shell/ape2mp3_wrapper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# usage:
+# $ find . -name "*.ape" ape2mp3_wrapper.sh {} \;
+# search all ape files in current directory and invoke ape2mp3.sh on each of 
+# them.
+
+if [[ $# < 1 ]];then
+	echo "please input ape filename"
+	exit 1
+fi
+
+DIRNAME=`dirname "$1"`
+
+cd "$DIRNAME"
+ape2mp3.sh "$1"
+
+echo -e "\E[32;47m" "=========================================================="
+reset
