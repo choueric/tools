d4f6714532c0b513ec0a889897bce195bf3412ae
zhs <haishan.zhou@magewell.com>
2017-01-09 13:53:23 +0800

    git_reauthor.sh: fix duplicate issue

--- shell/git_reauthor.sh
+++ shell/git_reauthor.sh
@@ -1,25 +1,11 @@
 #!/bin/sh
- 
-git filter-branch --env-filter '
- 
-an="$GIT_AUTHOR_NAME"
-am="$GIT_AUTHOR_EMAIL"
-cn="$GIT_COMMITTER_NAME"
-cm="$GIT_COMMITTER_EMAIL"
- 
-if [ "$GIT_COMMITTER_EMAIL" = "haishan.zhou@magewell.com" ]
-then
-    cn="choueric"
-    cm="zhssmail@gmail.com"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "haishan.zhou@magewell.com" ]
-then
-    an="choueric"
-    am="zhssmail@gmail.com"
-fi
- 
-export GIT_AUTHOR_NAME="$an"
-export GIT_AUTHOR_EMAIL="$am"
-export GIT_COMMITTER_NAME="$cn"
-export GIT_COMMITTER_EMAIL="$cm"
-'
+
+git filter-branch -f --commit-filter '
+        if [ "$GIT_AUTHOR_EMAIL" = "haishan.zhou@magewell.com" ];
+        then
+                GIT_AUTHOR_NAME="choueric";
+                GIT_AUTHOR_EMAIL="zhssmail@gmail.com";
+                git commit-tree "$@";
+        else
+                git commit-tree "$@";
+        fi' HEAD
