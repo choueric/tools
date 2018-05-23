e16df5f28f1ccc92d6d062d9755cd0e42da50256
choueric <zhssmail@gmail.com>
2018-05-23 15:29:15 +0800

    bit_operate: change to unit test

--- C/utils/bit_operate/main.c
+++ C/utils/bit_operate/main.c
@@ -6,13 +6,13 @@
 #include "bitop.h"
 #include "print.h"
 
-static void test_equal(uint8_t a, uint8_t b)
-{
-	if (a != b) {
-		printf("%#x != %#x\n", a, b);
-		exit(1);
-	}
-}
+#define test_equal(a, b) do { \
+	int aa = (a), bb = (b); \
+	if (aa != bb) { \
+		printf("%d: %#x != %#x\n", __LINE__, aa, bb);\
+		exit(1); \
+	} \
+} while (0)
 
 static void test_bit_mask()
 {
@@ -21,47 +21,53 @@ static void test_bit_mask()
 			test_equal(MASK(i, l), BIT_MASK(i, i + l-1));
 }
 
-int main()
+static void test_ffs()
 {
-	int ret; 
-	unsigned char s = BIT(3) | BIT(6) | BIT(0) | BIT(7);
-
-    printf("***** build time %s %s %d %s\n", __TIME__,__DATE__,__GNUC__,__VERSION__);
-
-    printf("ret = 0xf0 & bit(0) = %x\n", fieldValue(0xf0, BIT(0)));
-    printf("ret = 0xf0 & bit(1) = %x\n", fieldValue(0xf0, BIT(1)));
-    printf("ret = 0xf0 & bit(2) = %x\n", fieldValue(0xf0, BIT(2)));
-    printf("ret = 0xf0 & bit(3) = %x\n", fieldValue(0xf0, BIT(3)));
-    printf("ret = 0xf0 & bit(4) = %x\n", fieldValue(0xf0, BIT(4)));
-    printf("ret = 0xf0 & bit(5) = %x\n", fieldValue(0xf0, BIT(5)));
-    printf("ret = 0xf0 & bit(6) = %x\n", fieldValue(0xf0, BIT(6)));
-    printf("ret = 0xf0 & bit(7) = %x\n", fieldValue(0xf0, BIT(7)));
-
-    printf("ret = 0xf0 & bit(2|3) = %x\n", fieldValue(0xf0, BIT(2) | BIT(3)));
-    printf("ret = 0xf0 & bit(3|4) = %x\n", fieldValue(0xf0, BIT(3) | BIT(4)));
-    printf("ret = 0xf0 & bit(4|5) = %x\n", fieldValue(0xf0, BIT(4) | BIT(5)));
-    printf("ret = 0xf0 & bit(6|7) = %x\n", fieldValue(0xf0, BIT(6) | BIT(7)));
-
-    printf("mask(3, 3) = 0x%02x\n", MASK(3, 3));
-    printf("mask(3, 4) = 0x%02x\n", MASK(3, 4));
-    printf("mask(4, 4) = 0x%02x\n", MASK(4, 4));
-
-    printf("ret = 0xf0 & mask(2, 2) = %x\n", fieldValue(0xf0, MASK(2, 2)));
-    printf("ret = 0xf0 & mask(3, 2) = %x\n", fieldValue(0xf0, MASK(3, 2)));
-    printf("ret = 0xf0 & mask(4, 2) = %x\n", fieldValue(0xf0, MASK(4, 2)));
-    printf("ret = 0xf0 & mask(6, 2) = %x\n", fieldValue(0xf0, MASK(6, 2)));
-    printf("ret = 0xf0 & mask(3, 3) = %x\n", fieldValue(0xf0, MASK(3, 3)));
-    printf("ret = 0xf0 & mask(3, 4) = %x\n", fieldValue(0xf0, MASK(3, 4)));
-    printf("ret = 0xf0 & mask(4, 4) = %x\n", fieldValue(0xf0, MASK(4, 4)));
-
-	test_bit_mask();
-
 	char str_buf[128] = {0};
-	while (s) {
-		ret = ffs(s);
+	uint8_t s = BIT(3) | BIT(6) | BIT(0) | BIT(7);
+	int results[4] = {1, 4, 7, 8};
+
+	for (int i = 0; i < 4; i++) {
+		int ret = ffs(s);
+		test_equal(results[i], ret);
 		printf("%02x(%s) -> %d\n", s, bin_str(&s, 1, str_buf, 128), ret);
 		s &= ~BIT((ret - 1));
 	}
+}
+
+int main()
+{
+	printf("***** build time %s %s %d %s *****\n", __TIME__,__DATE__,__GNUC__,__VERSION__);
+
+    test_equal(fieldValue(0xf0, BIT(0)), 0);
+    test_equal(fieldValue(0xf0, BIT(1)), 0);
+    test_equal(fieldValue(0xf0, BIT(2)), 0);
+    test_equal(fieldValue(0xf0, BIT(3)), 0);
+    test_equal(fieldValue(0xf0, BIT(4)), 1);
+    test_equal(fieldValue(0xf0, BIT(5)), 1);
+    test_equal(fieldValue(0xf0, BIT(6)), 1);
+    test_equal(fieldValue(0xf0, BIT(7)), 1);
+
+    test_equal(fieldValue(0xf0, BIT(2) | BIT(3)), 0);
+    test_equal(fieldValue(0xf0, BIT(3) | BIT(4)), 2);
+    test_equal(fieldValue(0xf0, BIT(4) | BIT(5)), 3);
+    test_equal(fieldValue(0xf0, BIT(6) | BIT(7)), 3);
+
+    test_equal(MASK(3, 3), 0x38);
+    test_equal(MASK(3, 4), 0x78);
+    test_equal(MASK(4, 4), 0xf0);
+
+    test_equal(fieldValue(0xf0, MASK(2, 2)), 0);
+    test_equal(fieldValue(0xf0, MASK(3, 2)), 2);
+    test_equal(fieldValue(0xf0, MASK(4, 2)), 3);
+    test_equal(fieldValue(0xf0, MASK(6, 2)), 3);
+    test_equal(fieldValue(0xf0, MASK(3, 3)), 6);
+    test_equal(fieldValue(0xf0, MASK(3, 4)), 0xe);
+    test_equal(fieldValue(0xf0, MASK(4, 4)), 0xf);
+
+	test_bit_mask();
+
+	test_ffs();
 
     return 0;
 }
